24b7cffb42009b165faf7f97d10be23f99b9982b
david <david146182003@gmail.com>
2025-05-28 22:07:46 -0400

    lab

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+async function getUserData(id) {
+
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
+  const retrunValue = await central(id);
+
+  const returnValueDb1 = await db1(id);
+
+  const returnedValueVau = await vault(id);
+
+  console.log(retrunValue)
+  console.log(returnValueDb1)
+  console.log(returnedValueVau)
+
+  return {
+    id: id,
+    name: "",
+    userName: returnValueDb1.username,
+    email: '',
+    address: '',
+    phone: 0,
+    website: '',
+    company: {
+      name: returnValueDb1.company.name, 
+      catchPhrase: returnValueDb1.catchPhrase, 
+      bs: returnValueDb1.bs
+    }
+
+
+
+
+
+  }
+}
+
+console.log(getUserData(2))
+console.log(getUserData(3))
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(getUserData(1))
